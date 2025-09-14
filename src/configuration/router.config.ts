bbf2441034c6e5c8dc246a61101560e4130a7294
Muntazir Manan <muntazircodes@gmail.com>
2025-09-14 12:50:20 +0530

    destination route added

--- src/configuration/router.config.ts
+++ src/configuration/router.config.ts
@@ -3,9 +3,10 @@ import { Express } from 'express';
 import authRoutes from '../routes/auth.routes';
 import userRoutes from '../routes/user.routes';
 import weatherRoutes from '../routes/weather.routes';
+import destinationRoutes from '../routes/destination.routes';
 
 export const init = (app: Express) => {
-    // Health check endpoint for Render
+
     app.get('/api/health', (req, res) => {
         res.status(200).json({
             status: 'healthy',
@@ -17,4 +18,5 @@ export const init = (app: Express) => {
     app.use('/api/users', userRoutes);
     app.use('/api/auth', authRoutes);
     app.use('/api/weather', weatherRoutes);
+    app.use('api/destinations', destinationRoutes);
 };
